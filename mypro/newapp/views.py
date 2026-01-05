5683b5c3de76cb7a6ea8ace87dd6514e20595d3f
suvarnaks1 <suvarnaks2002@gmail.com>
2026-01-05 17:08:04 +0530

    lastchange

--- mypro/newapp/views.py
+++ mypro/newapp/views.py
@@ -11,8 +11,28 @@ def add(request):
 
 def addrec(request):
     x=request.POST['first']
-    y=request.POST['first']
-    z=request.POST['first']
+    y=request.POST['last']
+    z=request.POST['country']
     mem=Member(firstname=x,lastname=y,country=z)
+    mem.save()
+    return redirect("/")
+
+def delete(request,id):
+    mem=Member.objects.get(id=id)
+    mem.delete()
+    return redirect("/")
+
+def update(request,id):
+    mem=Member.objects.get(id=id)
+    return render(request,'update.html',{'mem':mem})
+
+def uprec(request,id):
+    x=request.POST['first']
+    y=request.POST['last']
+    z=request.POST['country']
+    mem=Member.objects.get(id=id)
+    mem.firstname=x
+    mem.lastname=y
+    mem.country=z
     mem.save()
     return redirect("/")
